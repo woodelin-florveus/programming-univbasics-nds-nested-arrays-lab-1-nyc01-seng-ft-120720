856250687c5f35f00b36d9f3aeb8915d9399c90a
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-12-01 21:35:20 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -37,8 +37,8 @@ def sorted_matrix
 end
 
 def matrix_lookup(matrix, row, column)
-  # binding.pry
-  row[column] = matrix
+  binding.pry
+
   # Given any matrix (array of arrays), a row index and a column index,
   # Return the matrix's content at that row and and column
 end
